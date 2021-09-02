aa9e1522a58ef201ec87c862bbb1284bf64355ee
marcoche88 <marco.che@hotmail.it>
2021-09-02 15:33:29 +0200

    added album cards

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
-console.log('Vue', Vue);
+/*
+Descrizione:
+Attraverso una chiamata ajax all’API di boolean:
+https://flynn.boolean.careers/exercises/api/array/music
+avremo a disposizione una decina di dischi musicali.
+Utilizzando vue, stampiamo a schermo una card per ogni album.
+BONUS:
+- Ordinare i dischi per anno di uscita.
+- Creare una select con tutti i generi dei dischi.
+  In base a cosa scegliamo nella select, vedremo i corrispondenti cd.
+*/
+
 
 Vue.config.devtools = true;
 
 const app = new Vue({
   el: '#app',
-  data: {},
+  data: {
+    discs: [],
+  },
   methods: {},
+  created() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((res) => {
+        this.discs = res.data.response;
+      });
+  },
 });
